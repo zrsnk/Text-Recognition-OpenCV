2a7c602410294f9b20c040ec60834f05b0f49392
Eri <66616258+zrsnk@users.noreply.github.com>
2022-10-10 15:48:39 +0900

    Generation files

---
+++ GenData2/GenData2/generateData.cpp
@@ -0,0 +1,87 @@
+﻿// 学習に使う画像とカテゴリーの対応情報と画像データをxmlファイルとして生成
+
+#include<opencv2/opencv.hpp>
+#include <opencv2/imgproc/types_c.h> // for CV_BGR2GRAY constant
+
+
+using namespace cv;
+using namespace std;
+
+const int MIN_CONTOUR_AREA = 70; // 細めのフォントでも確実に検出されるように
+const int RESIZED_IMAGE_WIDTH = 20;
+const int RESIZED_IMAGE_HEIGHT = 30;
+// 統一した大きさの四角内に文字を収めるので
+
+
+int main() {
+	Mat inputTrainingImg; // 読み込むtraining用画像
+	Mat grayScaledImg;
+	Mat thresholdedImg;
+	Mat clonedThresholdedImg;
+
+	// 輪郭検出用
+	vector<vector<Point>> contoursVector; // 検出される輪郭の座標
+	vector<Vec4i> vecHiearchy; // 階層構造の情報を保存した配列
+
+	Mat classificationForTraining; // 学習のための画像とカテゴリーの対応情報
+	Mat imagesForTrainingAsFlattenedFloat;
+
+	// 学習させるカテゴリーの限定
+	vector<int> categoriesOfInterest = { '1','7','2','J','Z','S' }; // waitKeyの返り値の型に合わせるためint型
+
+	inputTrainingImg = imread("training3.png");
+
+	// グレースケール化(二値化のために)
+	cvtColor(inputTrainingImg, grayScaledImg, CV_BGR2GRAY);
+
+	// 二値化処理(より高精度な輪郭抽出のため)
+	adaptiveThreshold(grayScaledImg, thresholdedImg, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 11, 2);
+	imshow("thresholded", thresholdedImg);
+
+	clonedThresholdedImg = thresholdedImg.clone(); // findContoursの際に上書きされてしまうため複製しておく
+
+	// 輪郭検出(一番外側の輪郭のみ,階層構造なし)
+	findContours(clonedThresholdedImg, contoursVector, vecHiearchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+	for (int i = 0; i < contoursVector.size(); i++) {
+		if (contourArea(contoursVector[i]) > MIN_CONTOUR_AREA) { // 小さすぎるものは無視
+			Rect rect = boundingRect(contoursVector[i]); 	// 検出した輪郭ベクトルから成る点群に外接する最小の長方形
+			rectangle(inputTrainingImg, rect, Scalar(0, 0, 255), 2); // 原画像に囲んでいる四角形を描画
+
+			Mat roi = thresholdedImg(rect); // 囲まれたregionのみinterested
+
+			Mat resizedRoi;
+			resize(roi, resizedRoi, Size(RESIZED_IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT)); // region of interestをすべて同じ面積にそろえる
+
+			imshow("roi", roi);
+			imshow("resized roi", resizedRoi);
+			imshow("原画像inputTrainingImg", inputTrainingImg); // キーボード入力する際に現在のroiが分かるようimshowする
+
+			int inputIntChar = waitKey(0); // キーボード入力した文字/数字をintとして返す
+
+			if (find(categoriesOfInterest.begin(), categoriesOfInterest.end(), inputIntChar) != categoriesOfInterest.end()) {
+				// もしキーボード入力したものがcategoriesOfInterestに含まれているものなら
+				classificationForTraining.push_back(inputIntChar);
+
+				Mat imgAsFloat;
+				resizedRoi.convertTo(imgAsFloat, CV_32FC1); // resizeしておいたroiをMat型からfloat型に変換
+
+				Mat imgAsFlattenedFloat = imgAsFloat.reshape(1, 1);
+
+				imagesForTrainingAsFlattenedFloat.push_back(imgAsFlattenedFloat);
+			}
+		}
+	}
+
+	// classificationForTrainingをclassification.xmlとして書き出す
+	FileStorage fileStorageClassifications("classifications.xml", FileStorage::WRITE);
+	fileStorageClassifications << "classfications" << classificationForTraining; 
+	fileStorageClassifications.release();
+
+	// imagesForTrainingAsFlattenedFloatをimages.xmlとして書き出す
+	FileStorage fileStorageImages("images.xml", FileStorage::WRITE);
+	fileStorageImages << "images" << imagesForTrainingAsFlattenedFloat;
+	fileStorageImages.release();
+
+	return 0;
+}
---
+++ GenData2/GenData2/trainTest.cpp
@@ -0,0 +1,67 @@
+﻿ #include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/opencv.hpp>
+ #include<opencv2/ml/ml.hpp>
+ #include<iostream>
+#include<sstream>
+#include <opencv2/imgproc/types_c.h> // for CV_BGR2GRAY constant
+
+using namespace cv;
+using namespace std;
+
+const int MIN_CONTOUR_AREA = 70;
+const int RESIZED_IMAGE_SIZE = 30;
+
+class ContourWithData {
+public:
+	vector<Point> contoursVector; // 検出した輪郭ベクトル
+	Rect rect; // 一番外側の輪郭に外接する四角形格
+	float contourArea; // 検出した輪郭の面積
+
+	bool isContourConsiderable() { // 小さすぎる面積の輪郭はfalseを返してはじく
+		return !(contourArea < MIN_CONTOUR_AREA);
+	}
+
+	// 囲んでいる四角を左からソート
+	static bool sortByRectCPosition(const ContourWithData& contour1, const ContourWithData& contour2) {
+		return (contour1.rect.x < contour2.rect.x); // x座標を比較して小さい方(左にある方)を返していく 
+	}
+};
+
+int main() {
+	// 1. サンプル画像データとカテゴリーを対応付けるよう学習
+
+	vector<ContourWithData> contoursWithData;
+	vector<ContourWithData> filteredContoursWithData;
+
+	Mat classificationForTraining; // classifications.xmlから読み込んだデータ
+	Mat imagesForTrainingAsFlattenedFloat; // images.xmlから読み込んだデータ
+
+	// 学習用データを読み込み
+	FileStorage classifications("classifications.xml", FileStorage::READ);
+	classifications["classifications"] >> classificationForTraining;
+	classifications.release();
+
+	FileStorage images("images.xml", FileStorage::READ);
+	images["images"] >> imagesForTrainingAsFlattenedFloat;
+	images.release();
+
+	Ptr<ml::KNearest> kNearest(ml::KNearest::create()); // kNearestオブジェクトをインスタンス化
+
+	// 集めた学習用データで学習
+	kNearest->train(imagesForTrainingAsFlattenedFloat, ml::ROW_SAMPLE, classificationForTraining);
+
+
+	// 2. 学習した(識別辞書が準備できた)ので与えられたテスト画像を識別処理
+	Mat inputTestingImg = imread("test1.png");
+
+	// 前処理
+	Mat grayScaledImg;
+	Mat thresholdedImg;
+	Mat clonedThresholdedImg;
+
+	cvtColor(inputTestingImg, grayScaledImg, CV_BGR2GRAY);
+
+
+	return 0;
+}
